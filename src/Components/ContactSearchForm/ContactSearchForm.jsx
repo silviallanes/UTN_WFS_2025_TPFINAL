acb50c450275aee376e31ac8f51fb3cf99d18acf
silviallanes <silviallanes@gmail.com>
2025-12-27 12:39:15 -0300

    se ajusta responsive

--- src/Components/ContactSearchForm/ContactSearchForm.jsx
+++ src/Components/ContactSearchForm/ContactSearchForm.jsx
@@ -6,11 +6,15 @@ export default function ContactSearchForm() {
     <div className="search-wrapper">
       <div className="search-bar">
         <span className="search-icon">
-          <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M15.009 13.805h-.636l-.22-.219a5.184 5.184 0 0 0 1.256-3.386 5.207 5.207 0 1 0-5.207 5.208 5.184 5.184 0 0 0 3.386-1.256l.219.22v.635l5.4 5.396 1.6-1.6-5.396-5.397ZM9.409 13.805a3.6 3.6 0 1 1 3.6-3.6 3.605 3.605 0 0 1-3.6 3.6Z"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.085.122l3.02 3.02a1 1 0 0 0 1.415-1.414l-3.02-3.02q-.06-.044-.122-.085zM6.5 11a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9" />
+        </svg>
         </span>
         <input type="text" placeholder="Buscar chat o iniciar uno nuevo" className="search-input" />
       </div>
+
      
+
     </div>
   )
 }
